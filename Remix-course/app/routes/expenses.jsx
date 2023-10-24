a0b805cd90ea855a97229f35a32b99f0039e06be
MichaelWiss <michael.wiss@gmail.com>
2023-10-24 13:28:41 -0500

    icons

--- Remix-course/app/routes/expenses.jsx
+++ Remix-course/app/routes/expenses.jsx
@@ -1,10 +1,18 @@
 // /expenses => shared layout
 import { Outlet } from '@remix-run/react';
+import expensesStyles from '~/styles/expenses.css';
+
 
 
 export default function ExpensesLayout() {
-    return <main>
+    return (
+    <main>
         <p>Shared element!</p>
         <Outlet />
     </main>
+    );
 }
+
+export function links() {
+    return [{ rel: 'stylesheet', href: expensesStyles }];
+  }
