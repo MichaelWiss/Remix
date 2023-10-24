7baccaf13728850f1c0221a41a850eaaea72ccb2
MichaelWiss <michael.wiss@gmail.com>
2023-10-24 14:35:53 -0500

    routing

--- Remix-course/app/routes/expenses/analysis.jsx
+++ Remix-course/app/routes/expenses/analysis.jsx
@@ -1,5 +1,7 @@
 // /expenses/analysis
 
+import ExpensesStatistics from '~/components/expenses/ExpenseStatistics';
+
 export default function ExpensesAnalysisPage() {
     return <h1>Expenses Analysis Page</h1>;
 }
