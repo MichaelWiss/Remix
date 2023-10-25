3ae806611d040312799f633a1b04c0ba0886459c
MichaelWiss <michael.wiss@gmail.com>
2023-10-25 17:15:53 -0500

    routes index pricing

--- Remix-course/app/routes/expenses.jsx
+++ Remix-course/app/routes/expenses.jsx
@@ -1,15 +1,33 @@
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'First Expense',
+        amount: 12.99,
+        date: new Date().toISOString(),
+    },
+    {
+        id: 'e2',
+        title: 'Second Expense',
+        amount: 15.99,
+        date: new Date().toISOString(),
+    },
+];
+
 // /expenses => shared layout
 import { Outlet } from '@remix-run/react';
 import expensesStyles from '~/styles/expenses.css';
+import ExpensesList from '~/components/expenses/ExpensesList';
 
 
 
 export default function ExpensesLayout() {
     return (
+    <>
+    <Outlet />
     <main>
-        <p>Shared element!</p>
-        <Outlet />
+        <ExpensesList expenses={DUMMY_EXPENSES} />
     </main>
+    </>
     );
 }
 
