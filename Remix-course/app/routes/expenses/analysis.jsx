a938a5c785a8f6461efa6578223a5e4724321498
MichaelWiss <michael.wiss@gmail.com>
2023-10-25 16:31:11 -0500

    chart in expenses analysis

--- Remix-course/app/routes/expenses/analysis.jsx
+++ Remix-course/app/routes/expenses/analysis.jsx
@@ -1,7 +1,27 @@
 // /expenses/analysis
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'First Expense',
+        amount: 12.99,
+        date: new Date().toISOString(),
+    },
+    {
+        id: 'e2',
+        title: 'Second Expense',
+        amount: 15.99,
+        date: new Date().toISOString(),
+    },
+];
 
 import ExpensesStatistics from '~/components/expenses/ExpenseStatistics';
+import Chart from '~/components/expenses/Chart';
 
 export default function ExpensesAnalysisPage() {
-    return <h1>Expenses Analysis Page</h1>;
+    return (
+    <main>
+            <Chart expenses={DUMMY_EXPENSES }/>
+            <ExpensesStatistics expenses={DUMMY_EXPENSES }/>
+    </main>
+    );
 }
