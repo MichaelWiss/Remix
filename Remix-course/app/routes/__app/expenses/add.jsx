d2ff8e1e5720212694a56d4bb59fb2709ee21d9f
MichaelWiss <michael.wiss@gmail.com>
2023-10-27 17:48:57 -0500

    connecting to MongoDB cloud

--- Remix-course/app/routes/__app/expenses/add.jsx
+++ Remix-course/app/routes/__app/expenses/add.jsx
@@ -1,7 +1,10 @@
 // /expenses/add
 import { useNavigate } from '@remix-run/react';
+import { redirect } from  '@remix-run/node';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { addExpense } from '~/data/expenses.server';
+
 
 
 export default function AddExpensesPage() {
@@ -16,6 +19,11 @@ export default function AddExpensesPage() {
     ) ;
 }
 
-export function action() {
-    
+export async function action({ request }) {
+    const formData = await request.formData();
+    const expenseData = Object.fromEntries(formData);
+    console.log(expenseData, formData);
+
+    await addExpense(expenseData);
+    return redirect('/expenses');
 }
