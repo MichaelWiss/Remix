4c60df0df396fc23bd2bfcbda6bb7869df44967b
MichaelWiss <michael.wiss@gmail.com>
2023-11-03 15:40:44 -0500

    updateExpense function

--- Remix-course/app/data/expenses.server.js
+++ Remix-course/app/data/expenses.server.js
@@ -33,3 +33,16 @@ export async function getExpense(id) {
     throw error;
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    await prisma.expense.update({
+      where: {id},
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    });
+  }
+}
