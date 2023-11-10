86b445676740ef3b49fa09b84bb7ad917e0c550f
MichaelWiss <michael.wiss@gmail.com>
2023-11-09 18:30:43 -0600

    action return statement

--- Remix-course/app/routes/__app/expenses/$id.jsx
+++ Remix-course/app/routes/__app/expenses/$id.jsx
@@ -45,6 +45,6 @@ export async function action({params, request}) {
     return redirect('/expenses');
     } else if(request.method === 'DELETE'){
         await deleteExpense(expenseId);
-        return ( deletedId: expenseId );
+        // return ( deletedId: expenseId );
     }
 }
